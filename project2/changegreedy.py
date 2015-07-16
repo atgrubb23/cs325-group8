2937c27cfdcbf05d1e17b1d8e155c23c3d124687
Melissa MacManiman <MacManimans@Melissas-iMac.local>
2015-07-15 22:33:14 -0700

    created changegreedy.py

---
+++ project2/changegreedy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#usage: python changegreedy.py
+###############################################################################
+from sys import maxint
+#similar to var_dump in PHP
+import pprint
+#function:	changegreedy
+#args:		integer array, integer
+#returns:	2 element list - first element is the number of coins, the second is an array that repressents the coins used.
+###############################################################################
+
